5f933e02610f205a1dc24e0d83f558137fefcd46
Zhack <zhacky@gmail.com>
2020-02-28 13:55:27 +0800

    sample data demo

---
+++ js/data.js
@@ -0,0 +1,32 @@
+var data = {
+    students: [
+        {
+            name: "Zaky",
+            age: "35",
+            section: "3A",
+            email: "zhacky@gmail.com",
+            grade: 78
+        },{
+            name: "Gary",
+            age: "35",
+            section: "3A",
+            email: "gary@gmail.com",
+            grade: 78
+        },
+    ]
+};
+
+var tableEl = document.getElementById("students-table");
+// var row = tableEl.insertRow(1);
+// row.innerHTML = "<td>test</td>";
+for (i = 0; i < data.students.length; i++) {
+    var std = data.students[i];
+    var row = tableEl.insertRow(i+1);
+    row.innerHTML = "<td>" + std.name + "</td>" +
+                    "<td>" + std.age + "</td>" +
+                    "<td>" + std.section + "</td>" +
+                    "<td>" + std.email + "</td>" +
+                    "<td>" + std.grade + "</td>";
+
+}
+console.log(tableEl.innerHTML);
